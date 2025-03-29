abcb6ad86ab40469c3a61adae4bb2cbeaed6ddba
BigSerj <sergeyconvent@gmail.com>
2025-03-29 14:29:17 +0300

    Render убарл app.py и исправил файлы для этого

--- app.py
+++ app.py
@@ -1,1 +1,15 @@
 # This file is deprecated. All functionality moved to appiframe.py
+
+import os
+from flask import Flask, render_template, request, send_file, jsonify, abort, Response
+
+app = Flask(__name__)
+
+# Add this near the top of the file after app creation
+port = int(os.environ.get("PORT", 10000))
+
+# ... rest of your existing code ...
+
+# Modify the if __name__ == '__main__': block at the bottom
+if __name__ == '__main__':
+    app.run(host='0.0.0.0', port=port)
--- wsgi.py
+++ wsgi.py
@@ -1,4 +1,4 @@
-from appiframe import app
+from app import app
 
 if __name__ == "__main__":
     app.run()
